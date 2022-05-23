bb4033aaf9fbdc078328e108b4c2e3d968b146b0
Amier Alchazli <amier1234@hotmail.com>
2022-05-23 09:28:48 +0200

    0.3.7
    - Removed unneeded code

--- DALmssqlServer/Database.cs
+++ DALmssqlServer/Database.cs
@@ -12,7 +12,7 @@ namespace DALmssqlServer
 {
     public class Database
     {
-        public static string connectionString = File.ReadAllText(@"C:\Users\amier\Desktop\Fontys\ICT & Software\S2\Bakka\JsonEncrypt.json");
+        public static string connectionString = File.ReadAllText(@"C:\Users\amier\OneDrive - Office 365 Fontys\S2\JsonEncrypt.json");
         public SqlConnection? conn;
         public Rootobject root;
 
