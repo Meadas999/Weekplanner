07440902707138f417608a710cadcd964c8afeb7
Amier Alchazli <amier1234@hotmail.com>
2022-05-31 21:59:34 +0200

    0.3.9.1
    -added methods to containers

--- WeekplannerClassesLibrary/VoedingContainer.cs
+++ WeekplannerClassesLibrary/VoedingContainer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using InterfaceLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,31 @@ namespace WeekplannerClassesLibrary
 {
     public class VoedingContainer
     {
+        private readonly IVoedingContainer container;
+
+        public VoedingContainer(IVoedingContainer container)
+        {
+            this.container = container;
+        }
+
+        public void AddVoeding(Voeding voeding, int userid)
+        {
+            container.AddVoeding(voeding.ToDTO(), userid);
+        }
+
+        public List<Voeding> GetAllvoedingenFrUser(int userid)
+        {
+            return container.GetAllVoedingFrUser(userid).Select(v => new Voeding(v)).ToList();
+        }
+
+        public void UpdateVoeding(Voeding voeding)
+        {
+            container.UpdateVoeding(voeding.ToDTO());
+        }
+
+        public void DeleteVoeding(int id)
+        {
+            container.DeleteVoeding(id);
+        }
     }
 }
