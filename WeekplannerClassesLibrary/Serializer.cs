8714755bff94c734e475aaca9a5cb591446f7b84
Amier Alchazli <amier1234@hotmail.com>
2022-06-01 13:13:48 +0200

    Fixed Random internal errors

---
+++ WeekplannerClassesLibrary/Serializer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeekplannerClassesLibrary
+{
+    public class Serializer
+    {
+        public class Rootobject
+        {
+            public Databaseconfig DatabaseConfig { get; set; }
+            public Defaults Defaults { get; set; }
+        }
+        public class Databaseconfig
+        {
+            public string Server { get; set; }
+            public string Database { get; set; }
+            public string User { get; set; }
+            public string Password { get; set; }
+            public string ConnectionString { get; set; }
+        }
+        public class Defaults
+        {
+            public string Standaardkleur { get; set; }
+        }
+    }
+}
